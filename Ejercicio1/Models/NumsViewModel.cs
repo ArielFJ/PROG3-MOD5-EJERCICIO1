047de13bfcc44efd87518f6314e191498bd82810
Ariel <arielfj@magnanym.com>
2020-02-24 17:17:44 -0400

    Modelo y vista principal agregados

---
+++ Ejercicio1/Models/NumsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Models
+{
+    public class NumsViewModel
+    {
+        public List<int> NumerosDecimales { get; set; } = new List<int>();
+        public List<string> NumerosBinarios { get; set; } = new List<string>();
+    }
+}
